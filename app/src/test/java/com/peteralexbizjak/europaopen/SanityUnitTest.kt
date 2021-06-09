03ef3c096ea4deefad8ea0a1e0b4ba6cebc97020
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-06-09 14:53:37 +0200

    Initial commit

---
+++ app/src/test/java/com/peteralexbizjak/europaopen/SanityUnitTest.kt
@@ -0,0 +1,11 @@
+package com.peteralexbizjak.europaopen
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SanityUnitTest {
+    @Test
+    fun universeWorks() {
+        assertEquals(4, 2 + 2)
+    }
+}
