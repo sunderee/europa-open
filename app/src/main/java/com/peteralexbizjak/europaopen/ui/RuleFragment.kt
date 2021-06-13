928579d1b26a8c1cd4a62eac33f55bce2e7b21ea
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-06-13 09:49:22 +0200

    Bind adapter to the rule fragment

--- app/src/main/java/com/peteralexbizjak/europaopen/ui/RuleFragment.kt
+++ app/src/main/java/com/peteralexbizjak/europaopen/ui/RuleFragment.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.peteralexbizjak.europaopen.databinding.FragmentRuleBinding
+import com.peteralexbizjak.europaopen.ui.adapters.RuleAdapter
+import com.peteralexbizjak.europaopen.ui.viewmodels.MeasuresViewModel
 
 class RuleFragment : Fragment() {
     private var bindingInstance: FragmentRuleBinding? = null
     private val binding get() = bindingInstance!!
+
+    private var recyclerViewAdapter: RuleAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val rulesData = arguments?.getParcelableArrayList<MeasuresViewModel.Rule>("data")
+        recyclerViewAdapter = rulesData?.let { RuleAdapter(it) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +33,9 @@ class RuleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fragmentRuleRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = recyclerViewAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/peteralexbizjak/europaopen/ui/adapters/RuleAdapter.kt
@@ -0,0 +1,33 @@
+package com.peteralexbizjak.europaopen.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.peteralexbizjak.europaopen.databinding.RulesListItemBinding
+import com.peteralexbizjak.europaopen.ui.viewmodels.MeasuresViewModel
+import java.util.ArrayList
+
+internal class RuleAdapter(
+    private val ruleData: ArrayList<MeasuresViewModel.Rule>
+) : RecyclerView.Adapter<RuleAdapter.ViewHolder>() {
+
+    internal class ViewHolder(
+        private val binding: RulesListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(rule: MeasuresViewModel.Rule) {
+            binding.title = rule.title
+            binding.contents = rule.comment
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemBinding = RulesListItemBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(ruleData[position])
+
+    override fun getItemCount(): Int = ruleData.size
+}
