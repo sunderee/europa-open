c02099dc1d3a4390148f551ca88882b3982b6cf6
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-06-22 16:01:50 +0200

    Improve navigation

--- app/src/main/java/com/peteralexbizjak/europaopen/ui/statistics/StatisticsInfoFragment.kt
+++ app/src/main/java/com/peteralexbizjak/europaopen/ui/statistics/StatisticsInfoFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.peteralexbizjak.europaopen.R
 import com.peteralexbizjak.europaopen.databinding.FragmentStatisticsInfoBinding
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
@@ -21,6 +23,13 @@ class StatisticsInfoFragment : Fragment() {
     ): View {
         bindingInstance = FragmentStatisticsInfoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentStatisticsInfoToolbar.setNavigationOnClickListener {
+            binding.root.findNavController().popBackStack(R.id.landingFragment, false)
+        }
     }
 
     override fun onDestroyView() {
