b9895d2472f9d4cb209ae47be266aaf041cec8d1
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-06-22 18:05:14 +0200

    Display data for regions

---
+++ app/src/main/java/com/peteralexbizjak/europaopen/ui/adapters/RegionsAdapter.kt
@@ -0,0 +1,40 @@
+package com.peteralexbizjak.europaopen.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.peteralexbizjak.europaopen.api.models.region.RegionModel
+import com.peteralexbizjak.europaopen.databinding.RegionListItemBinding
+
+internal class RegionsAdapter : RecyclerView.Adapter<RegionsAdapter.ViewHolder>() {
+    private val regionData = mutableListOf<RegionModel>()
+
+    internal class ViewHolder(
+        private val binding: RegionListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(regionModel: RegionModel) {
+            binding.region = regionModel.region
+            binding.color = regionModel.color
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemBinding = RegionListItemBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(regionData[position])
+
+    override fun getItemCount(): Int = regionData.size
+
+    fun setNewData(newData: List<RegionModel>) {
+        notifyDataSetChanged()
+        regionData.apply {
+            clear()
+            addAll(newData)
+        }
+    }
+}
--- app/src/main/java/com/peteralexbizjak/europaopen/ui/statistics/StatisticsInfoFragment.kt
+++ app/src/main/java/com/peteralexbizjak/europaopen/ui/statistics/StatisticsInfoFragment.kt
@@ -6,15 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.peteralexbizjak.europaopen.R
 import com.peteralexbizjak.europaopen.databinding.FragmentStatisticsInfoBinding
+import com.peteralexbizjak.europaopen.models.GenericResponse
+import com.peteralexbizjak.europaopen.ui.adapters.RegionsAdapter
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class StatisticsInfoFragment : Fragment() {
     private var bindingInstance: FragmentStatisticsInfoBinding? = null
     private val binding get() = bindingInstance!!
 
+    private lateinit var country: String
+
     private val statisticsViewModel by sharedViewModel<StatisticsViewModel>()
+
+    private val recyclerViewAdapter = RegionsAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            country = it.getString("country") as String
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +42,34 @@ class StatisticsInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.fragmentStatisticsInfoContent.visibility = View.GONE
         binding.fragmentStatisticsInfoToolbar.setNavigationOnClickListener {
             binding.root.findNavController().popBackStack(R.id.landingFragment, false)
+        }
+
+        binding.fragmentStatisticsInfoRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = recyclerViewAdapter
+        }
+
+        binding.country = country
+
+        statisticsViewModel.regionData.observe(viewLifecycleOwner) {
+            when (it) {
+                is GenericResponse.Success -> {
+                    binding.apply {
+                        fragmentStatisticsInfoContent.visibility = View.VISIBLE
+                        fragmentStatisticsInfoProgressBar.visibility = View.GONE
+                    }
+                    recyclerViewAdapter.setNewData(it.data)
+                }
+                is GenericResponse.Error -> {
+                    Snackbar.make(binding.root, it.exception, Snackbar.LENGTH_SHORT).show()
+                }
+                is GenericResponse.Loading -> {
+                    Snackbar.make(binding.root, "Loading", Snackbar.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
