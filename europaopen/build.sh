d8f3e06c5cc9a6c16080bda142acf4136a55e40b
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2021-11-27 17:04:20 +0100

    Pre-deploy changes

---
+++ europaopen/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+debug_print() {
+    STARTCOLOR="\e[92m";
+    ENDCOLOR="\e[0m";
+
+    printf "\n$STARTCOLOR%b$ENDCOLOR\n" "$1";
+}
+
+debug_print "Cleaning up + installing dependencies..."
+flutter clean
+flutter pub get
+flutter packages pub run build_runner build --delete-conflicting-outputs
+
+debug_print "Building Android's App Bundle..."
+flutter build appbundle --obfuscate --split-debug-info=./debug
